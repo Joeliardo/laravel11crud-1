a9e72010dc35dd117ece2a38c0397c2ee9f7e402
Reinert Yosua <43610022+ReinertYosua@users.noreply.github.com>
2024-07-08 16:32:11 +0700

    Update index.blade.php

--- resources/views/admin/pemeriksaan/index.blade.php
+++ resources/views/admin/pemeriksaan/index.blade.php
@@ -8,7 +8,7 @@
         @include('partial.successalert')
             <div class="card border-0 shadow rounded">
                 <div class="card-body">
-                    <a href="{{route('pemeriksaan.create')}}" class="btn btn-primary">Pemeriksaan Baru</a>
+                    <a href="" class="btn btn-primary">Pemeriksaan Baru</a>
                     <table class="table table-bordered mt-1 text-center mt-3">
                         <thead>
                         <tr>
@@ -23,19 +23,19 @@
                         </tr>
                         </thead>
                         <tbody>
-                            @forelse($datapemeriksaan as $dp)
+                            
                                 <tr>
-                                    <td>{{ $dp->no_transaksi_pemeriksaan }}</td>
-                                    <td>{{ $dp->namaPasien }}</td>
-                                    <td>{{ $dp->namaDokter }}</td>
-                                    <td>{{ $dp->spesialisasi }}</td>
-                                    <td>{{ $dp->tanggalPeriksa }}</td>
-                                    <td>{{ $dp->keluhan }}</td>
-                                    <td><a href="{{ route('admin.file.download', $dp->fileLampiran) }}" class="btn btn-sm btn-primary"> {{ $dp->fileLampiran }}</a></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td><a href="" class="btn btn-sm btn-primary"> File</a></td>
                                     <td>
                                         <div class="btn-group" role="group" aria-label="Aksi">
-                                            <a href="{{ route('pemeriksaan.edit', $dp->id) }}" class="btn btn-sm btn-primary">EDIT</a>
-                                            <form onsubmit="return confirm('Apakah Anda Yakin ?');" action="{{ route('pemeriksaan.destroy', $dp->id) }}" method="POST">
+                                            <a href="" class="btn btn-sm btn-primary">EDIT</a>
+                                            <form onsubmit="return confirm('Apakah Anda Yakin ?');" action="" method="POST">
                                                 @csrf
                                                 @method('DELETE')
                                                 <button type="submit" class="btn btn-sm btn-danger">DELETE</button>
@@ -51,7 +51,7 @@
                             @endforelse
                         </tbody>
                     </table>
-                    {{ $datapemeriksaan->links() }}
+                    
                 </div>
             </div>
         </div>
